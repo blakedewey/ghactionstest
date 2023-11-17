a631684fda87303c3895ba1a72fbdb34108bfd69
Blake Dewey <blake.dewey@jhu.edu>
2023-11-17 10:54:00 -0500

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+ARG PYTHON_VERSION=3.11.6
+ARG DEBIAN_VERSION=bullseye
+
+FROM python:${PYTHON_VERSION}-slim-${DEBIAN_VERSION}
+LABEL maintainer=blake.dewey@jhu.edu
+ARG PYTHON_VERSION
+ARG DEBIAN_VERSION
+
+ENV PYTHONUSERBASE=/opt/python
+
+RUN apt-get update && \
+    apt-get -y --no-install-recommends install git && \
+    rm -rf /var/lib/apt/lists/*
+
+# Create manifest json file
+RUN echo -e "{\n \
+    \"PYTHON_VERSION\": \"${PYTHON_VERSION}\",\n \
+    \"DEBIAN_VERSION\": \"${DEBIAN_VERSION}\"\n \
+}" > /opt/manifest.json
+
+# Copy package and install
+COPY . /tmp/ghactionstest-src/
+RUN pip install --no-cache-dir /tmp/ghactionstest-src/ && \
+    rm -rf /tmp/ghactionstest-src/
+
+ENTRYPOINT ["python"]
---
+++ setup.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+__package_name__ = "ghactionstest"
+
+
+def get_version_and_cmdclass(pkg_path):
+    """Load version.py module without importing the whole package.
+
+    Template code from miniver
+    """
+    import os
+    from importlib.util import module_from_spec, spec_from_file_location
+
+    spec = spec_from_file_location("version", os.path.join(pkg_path, "_version.py"))
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module.__version__, module.get_cmdclass(pkg_path)
+
+
+__version__, cmdclass = get_version_and_cmdclass(__package_name__)
+
+setup(
+    name=__package_name__,
+    version=__version__,
+    description="Test package for GitHub Actions",
+    long_description=(Path(__file__).parent.resolve() / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="Blake Dewey",
+    author_email="blake.dewey@jhu.edu",
+    url="https://github.com/blakedewey/ghactionstest",
+    license="Apache License, 2.0",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering",
+    ],
+    packages=find_packages(),
+    keywords="testing",
+    python_requires=">=3.10",
+    cmdclass=cmdclass,
+)
